d45b17f8ce301405b111247c6f6fcf1e7d4abc4e
sonalkuchekar <38507834+sonalkuchekar@users.noreply.github.com>
2018-06-13 20:19:26 +0530

    Add files via upload

---
+++ lights.ino
@@ -0,0 +1,48 @@
+int Relay1 = D4;
+int pir1 = D1;
+int pir2 = D2;
+int state1;
+int state2;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(Relay1, OUTPUT);
+  pinMode(pir1, INPUT);
+  pinMode(pir2, INPUT);
+  digitalWrite(Relay1,LOW);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  state1 = digitalRead(pir1);
+  state2 = digitalRead(pir2);
+
+  if (state1 == HIGH) {
+    Serial.println("motion detected in pir 1");
+
+  } else {
+    Serial.println("motion still in pir 1");
+
+  }
+
+  if (state2 == HIGH) {
+    Serial.println("Motion detected in pir 2");
+  } else {
+    Serial.println("Motion still in pir 2");
+  }
+
+  if (state1 == HIGH || state2 == HIGH )
+  {
+    digitalWrite(Relay1, LOW);
+
+  }
+  else
+  {
+    digitalWrite(Relay1, HIGH);
+  }
+ 
+ 
+}
+
